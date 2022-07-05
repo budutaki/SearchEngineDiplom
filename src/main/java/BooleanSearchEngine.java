6a119f67c6b48a553c8789cf376c51fcdfae990e
budutaki <takibudu@gmail.com>
2022-07-05 18:28:16 +0300

    BooleanSearchEngine class finished

--- src/main/java/BooleanSearchEngine.java
+++ src/main/java/BooleanSearchEngine.java
@@ -1,49 +1,52 @@
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfPage;
+import com.itextpdf.kernel.pdf.PdfReader;
+import com.itextpdf.kernel.pdf.canvas.parser.PdfTextExtractor;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.*;
 
 public class BooleanSearchEngine implements SearchEngine {
-    //??? мапа
+    public Map<String, List<PageEntry>> index;
 
     public BooleanSearchEngine(File pdfsDir) throws IOException {
-//
-//        Чтобы создать объект пдф-документа, нужно указать объект File
-//        этого документа следующим классам: var doc = new PdfDocument(new PdfReader(pdf));.
-//        Чтобы получить объект одной страницы документа, нужно вызвать doc.getPage(номерСтраницы).
-//        Полистайте методы doc чтобы найти способ узнать количество страниц в документе.
-//        Чтобы получить текст со страницы, используйте var text = PdfTextExtractor.getTextFromPage(page);.
-//        Чтобы разбить текст на слова (а они в этих документах разделены могут быть не только пробелами),
-//        используйте var words = text.split("\\P{IsAlphabetic}+");.
-//
-//        Сканируя каждый пдф-файл вы перебираете его страницы,
-//        для каждой страницы извлекаете из неё слова и подсчитываете их количество.
-//        После чего, для каждого уникального слова создаёте объект PageEntry и сохраняете
-//        в поле (а что за поле - предлагалось подумать выше).
-//        Учтите также, что мы хотим регистронезависимый поиск, т.е. по слову "бизнес"
-//        должны учитываться и "бизнес", и "Бизнес" в документах
-//        (для этого при обработке достаточно каждое слово переводить в нижний регистр
-//        с помощью встроенного метода класса String для этих целей).
-//
-//          Для подсчёта частоты слов можно использовать следующий приём
-//        Map<String, Integer> freqs = new HashMap<>();
-//        for (var word : words) {
-//            if (word.isEmpty()) {
-//                continue;
-//            }
-//            freqs.put(word.toLowerCase(), freqs.getOrDefault(word.toLowerCase(), 0) + 1);
-//        }
-//        Также, списки ответов для каждого слова должны быть отсортированы в порядке уменьшения
-//        поля count.
-//
+        index = new HashMap<>();
+        File[] pdfs = pdfsDir.listFiles();
+        for (File pdf : pdfs) {
+            var doc = new PdfDocument(new PdfReader(pdf));
+            for (int i = 0; i < doc.getNumberOfPages(); i++) {
+                PdfPage page = doc.getPage(i + 1);
+                var text = PdfTextExtractor.getTextFromPage(page);
+                var words = text.split("\\P{IsAlphabetic}+");
 
+                Map<String, Integer> freqs = new HashMap<>();
+                for (var word : words) {
+                    if (word.isEmpty()) {
+                        continue;
+                    }
+                    freqs.put(word.toLowerCase(), freqs.getOrDefault(word.toLowerCase(), 0) + 1);
+                }
 
-
+                for (Map.Entry<String, Integer> entries : freqs.entrySet()) {
+                    PageEntry entry = new PageEntry(pdf.getName(), doc.getPageNumber(page), entries.getValue());
+                    if (index.containsKey(entries.getKey())) {
+                        index.get(entries.getKey()).add(entry);
+                    } else {
+                        List<PageEntry> pageEntryList = new ArrayList<>();
+                        pageEntryList.add(entry);
+                        index.put(entries.getKey(), pageEntryList);
+                    }
+                }
+            }
+        }
 
     }
 
     @Override
     public List<PageEntry> search(String word) {
-        // тут реализуйте поиск по слову
-        return Collections.emptyList();
+        List<PageEntry> result = index.get(word);
+        Collections.sort(result);
+        return result;
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,17 @@
 import java.io.File;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         BooleanSearchEngine engine = new BooleanSearchEngine(new File("pdfs"));
         System.out.println(engine.search("бизнес"));
+//        for (Map.Entry<String, List<PageEntry>> entries : engine.index.entrySet()) {
+//            System.out.println(entries.getValue().size());
+//        }
+
+//        System.out.println(engine.search("бизнес"));
 
         // здесь создайте сервер, который отвечал бы на нужные запросы
         // слушать он должен порт 8989
